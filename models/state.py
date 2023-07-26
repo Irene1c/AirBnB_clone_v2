b80da2ef1bb8a66ecce336367cc9218046182a33
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-07-26 15:09:18 -0700

    remove whitespaces

--- models/state.py
+++ models/state.py
@@ -12,8 +12,8 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    
-    if os.environ['HBNB_TYPE_STORAGE'] == 'db': 
+
+    if os.environ['HBNB_TYPE_STORAGE'] == 'db':
         cities = relationship('City', backref='state', cascade='all,delete')
     else:
         @property
