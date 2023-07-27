fbb0536aaca26d38858eca7918d56d7f0fa514e9
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-07-27 01:25:59 -0700

    rendering List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page inside body tag te States sorted by name,
+        in the format: <state.id>: <B><state.name>
+    """
+
+    states = storage.all(State).values()
+    return render_template('states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """remove the current SQLAlchemy Session after each request"""
+
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
